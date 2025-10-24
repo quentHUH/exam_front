c013d7c5be7629e7c2829473c9713fab27d20c95
QuentinB <quentin.bourdois@supdevinci-edu.fr>
2025-10-24 14:05:33 +0200

    refactor: remove unnecessary user details from UserCard component

--- src/components/UserCard.tsx
+++ src/components/UserCard.tsx
@@ -12,9 +12,6 @@ export default function UserCard({ user }: Props) {
       <div className="user-info">
         <h3>{user.firstName} {user.lastName}</h3>
         <p className="email">{user.email}</p>
-        <p className="meta">Age: {user.age} · {user.company?.name ?? '-'}</p>
-        <p className="address">{user.address?.city ?? ''}{user.address ? `, ${user.address.country}` : ''}</p>
-        <p className="phone">{user.phone}</p>
       </div>
     </div>
   )
