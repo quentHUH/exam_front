4f8e312ac183af8555f98acd5e33b2de3e8fabc0
QuentinB <quentin.bourdois@supdevinci-edu.fr>
2025-10-26 14:05:30 +0100

    feat: testing page management

--- src/components/UserList.tsx
+++ src/components/UserList.tsx
@@ -11,6 +11,10 @@ export const UserList = () => {
   const [error, setError] = useState<string | null>(null);
   const [search, setSearch] = useState<string>("");
   const [sortBy, setSortBy] = useState<"name" | "age">("name")
+  const [currentPage, setCurrentPage] = useState<number>(1)
+  const usersPerPage = 9 //j'ai mis 9 et non 10 comme dans l'énoncé car j'ai des lignes de 3 cards dans la grille
+  // et ce serait plus esthétique avec 3 lignes pleines
+
 
 
   useEffect(() => {
@@ -41,39 +45,63 @@ export const UserList = () => {
     })
 
 
+  const indexOfLastUser = currentPage * usersPerPage
+  const indexOfFirstUser = indexOfLastUser - usersPerPage
+  const currentUsers = filteredUsers.slice(indexOfFirstUser, indexOfLastUser)
+  const totalPages = Math.ceil(filteredUsers.length / usersPerPage)
+
+  const handlePageChange = (page: number) => {
+    setCurrentPage(page)
+  }
+
+
+
 
   if (loading) return <div className="app"><h1>Chargement...</h1></div>
   if (error) return <div className="app"><h1>Erreur: {error}</h1></div>
   if (users.length === 0) return <div className="app"><p>Aucun utilisateur trouvé.</p></div>
 
 
-  return (
+
+return (
     <div className="app">
       <h1>Liste des utilisateurs</h1>
-
 
       <div className="controls">
         <input
           type="text"
           placeholder="Rechercher par nom, prénom ou email"
           value={search}
-          onChange={(e) => setSearch(e.target.value)}
+          onChange={(e) => {
+            setSearch(e.target.value)
+            setCurrentPage(1) 
+          }}
         />
 
-        <select value={sortBy} onChange={(e) => setSortBy(e.target.value as "name" | "age")}>
+<select value={sortBy} onChange={(e) => setSortBy(e.target.value as "name" | "age")}>
           <option value="name">Trier par nom</option>
           <option value="age">Trier par âge</option>
         </select>
       </div>
 
-
-
       <div className="users-grid">
-        {filteredUsers.map(user => (
+        {currentUsers.map(user => (
           <UserCard key={user.id} user={user} />
         ))}
       </div>
+
+      
+      <div className="pagination">
+        {Array.from({ length: totalPages }, (_, i) => (
+          <button key={i}
+            className={currentPage === i + 1 ? "active" : ""}
+            onClick={() => handlePageChange(i + 1)}>
+            {i + 1}
+          </button>
+        ))}
+      </div>
     </div>
+  //tous les bouton sont mis a la suite pour naviguer plus rapidement je changerais peut-etre pas la suite
   )
 }
 
