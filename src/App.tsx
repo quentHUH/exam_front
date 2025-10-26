2c8037c74b72e704443e35867dac4b88294771fd
QuentinB <quentin.bourdois@supdevinci-edu.fr>
2025-10-26 18:11:57 +0100

    feat: added theme switching functionality with light and dark modes

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,32 @@ import './styles.css'
 //import './App.css';
 import { UserList } from './components/UserList'
 import UserDetail from './components/UserDetail'
+import { useState, useEffect } from 'react'
 
 function App() {
-  return (
+  const [theme, setTheme] = useState<'light' | 'dark'>('light')
 
-    <BrowserRouter>
+  useEffect(() => {
+    document.body.className = `theme-${theme}`
+  }, [theme])
+
+  const toggleTheme = () => {
+    setTheme(prev => (prev === 'light' ? 'dark' : 'light'))
+  }
+
+  return (
+    <div className="app">
+      <button className='action-btn theme-btn' onClick={toggleTheme}>
+        Passer en mode {theme === 'light' ? 'sombre' : 'clair'}
+      </button>
+
+      {<BrowserRouter>
       <Routes>
         <Route path="/" element={<UserList />} />
         <Route path="/users/:id" element={<UserDetail />} />
       </Routes>
-    </BrowserRouter>
-
+    </BrowserRouter>}
+    </div>
   )
 }
 
